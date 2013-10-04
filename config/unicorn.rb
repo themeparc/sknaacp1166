f0b59695eda02c1d7f7a83cbf87598e1177cb05c
Eric Paxton <heypaxton@gmail.com>
2013-10-03 19:29:26 -0700

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.sknaacp1166.sock"
 worker_processes 2
 timeout 30
